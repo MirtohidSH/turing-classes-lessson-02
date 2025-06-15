27d03d29f26ffc4f882f2ff5458225767e8aa845
MirtohidSH <mirtohid444@example.com>
2025-06-15 21:40:53 +0400

    Month Commit - 2

--- src/main/java/org/example/Season.java
+++ src/main/java/org/example/Season.java
@@ -6,7 +6,7 @@ public class Season {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int month = sc.nextInt();
-        System.out.println(month <=2  ? "Winter" : month <=5 ? "Spring" :
+        System.out.println(month < 0 ? "Wrong Month" : month <=2  ? "Winter" : month <=5 ? "Spring" :
                 month <=8 ? "Summer" : month <=11 ? "Autumn" : month ==12 ? "Winter" : "Wrong Month");
     }
 }
