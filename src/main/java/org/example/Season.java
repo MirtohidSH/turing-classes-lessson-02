16417e1b0a565a2322e57d1ba44de5bb4fdc0f53
MirtohidSH <mirtohid444@example.com>
2025-06-17 22:36:07 +0400

    Month Commit - 3

--- src/main/java/org/example/Season.java
+++ src/main/java/org/example/Season.java
@@ -8,5 +8,13 @@ public class Season {
         int month = sc.nextInt();
         System.out.println(month < 0 ? "Wrong Month" : month <=2  ? "Winter" : month <=5 ? "Spring" :
                 month <=8 ? "Summer" : month <=11 ? "Autumn" : month ==12 ? "Winter" : "Wrong Month");
+
+//        int ay = sc.nextInt();
+//        String fəsil = (ay >= 3 && ay <= 5) ? "Yaz" :
+//                       (ay >= 6 && ay <= 8) ? "Yay" :
+//                       (ay >= 9 && ay <= 11) ? "Payız" :
+//                       (ay == 12 || ay == 1 || ay == 2) ? "Qış" : "Yanlış ay";
+//
+//        System.out.println(fəsil);
     }
 }
