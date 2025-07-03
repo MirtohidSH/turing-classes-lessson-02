5a40e36bcc21e8923ece9f7e0fa0ddc564cf1a10
MirtohidSH <mirtohid444@example.com>
2025-07-03 21:37:20 +0400

    Ex commit

---
+++ src/main/java/org/example/Ex.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ex {
+    public static void main(String[] args) {
+//        String a = "aaa";
+//        String[] b = a.split("a");
+//        System.out.println("Length: " + b.length);
+
+    }
+}
